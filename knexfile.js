b9c984ed4e0ddf4ad41c047e02427da980f67df0
treemmett <tregan@tregan.me>
2018-10-27 01:17:02 -0600

    Adds registration route

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/verupt',
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: __dirname + '/lib/db/migrations'
     },
     seeds: {
-      directory: __dirname + '/db/seeds'
+      directory: __dirname + '/lib/db/seeds'
     }
   },
 
@@ -14,10 +14,10 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: __dirname + '/lib/db/migrations'
     },
     seeds: {
-      directory: __dirname + '/db/seeds'
+      directory: __dirname + '/lib/db/seeds'
     }
   }
 }
